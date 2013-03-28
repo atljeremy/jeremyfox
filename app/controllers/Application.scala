47a827a5732cd27ce29c511bdcba0b186ef90cfb
Jeremy Fox <atljeremy@me.com>
2013-03-28 13:40:53 -0400

    Adding Mixpanel image to footer

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import play.api._
+import libs.concurrent.Akka
 import play.api.mvc._
 
 object Application extends Controller {
@@ -8,5 +9,16 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Chose A Repo"))
   }
-  
+
+//  def index = Action {
+//    val promiseOfIntent = Akka.future { intensiveComputation() }
+//    Async {
+//      promiseOfIntent.orTimeout("Oops", 1000).map { eitherIntentOrTimeout =>
+//        eitherIntentOrTimeout.fold(
+//          timeout => InternalServerError(timeout),
+//          i => Ok("Got result: " + i)
+//        )
+//      }
+//    }
+//  }
 }
